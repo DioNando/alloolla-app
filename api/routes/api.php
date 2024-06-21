0c6cbc67526cbe179b37c1d4af7cdc32aad39bd9
David Fernando <mahefa.fernando@gmail.com>
2024-06-21 23:56:05 +0100

    Feature(api): add authentification

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Grouped routes requiring authentication
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+// Public routes for authentication
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// API v1 routes, protected by auth:sanctum middleware
+// Route::group(['namespace' => 'App\Http\Controllers\Api', 'middleware' => 'auth:sanctum'], function() {
+    // Route::apiResource('articles', ArticleController::class);
+// });
